5b46c58cb1a493e5d5984bcd3ba7e11cf0e516be
isaiasneto <isaiasneto@gmail.com>
2021-04-15 21:07:30 -0300

    add student service methods

--- src/student/student.service.ts
+++ src/student/student.service.ts
@@ -1,4 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateStudentInput } from './create-student.input';
+import { StudentEntity } from './student.entity';
+import { v4 as uuid } from 'uuid';
 
 @Injectable()
-export class StudentService {}
+export class StudentService {
+  constructor(
+    @InjectRepository(StudentEntity)
+    private studentRepository: Repository<StudentEntity>,
+  ) {}
+
+  async getStudents(): Promise<StudentEntity[]> {
+    return this.studentRepository.find();
+  }
+
+  async createStudent(
+    createStudentInput: CreateStudentInput,
+  ): Promise<StudentEntity> {
+    const { firstName, lastName } = createStudentInput;
+    const student = this.studentRepository.create({
+      id: uuid(),
+      firstName,
+      lastName,
+      createdAt: new Date().toISOString(),
+    });
+
+    return this.studentRepository.save(student);
+  }
+}
