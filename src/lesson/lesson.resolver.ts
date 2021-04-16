1fd265eb9421e0cd0ff405ff4c28fc3ce74ea863
isaiasneto <isaiasneto@gmail.com>
2021-04-15 22:09:35 -0300

    Add mutation assign students

--- src/lesson/lesson.resolver.ts
+++ src/lesson/lesson.resolver.ts
@@ -1,28 +1,53 @@
-/* eslint-disable prettier/prettier */
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import {
+  Args,
+  Mutation,
+  Parent,
+  Query,
+  ResolveField,
+  Resolver,
+} from '@nestjs/graphql';
+import { StudentService } from '../student/student.service';
+import { AssignStudentsToLessonInput } from './assing-students-to-lesson.input';
+import { LessonEntity } from './lesson.entity';
 import { CreateLessonInput } from './lesson.input';
 import { LessonService } from './lesson.service';
 import { LessonType } from './lesson.type';
 
-@Resolver(of => LessonType)
+@Resolver(() => LessonType)
 export class LessonResolver {
-  constructor(private lessonService: LessonService) {}
+  constructor(
+    private lessonService: LessonService,
+    private studentService: StudentService,
+  ) {}
 
-  @Query(type => [LessonType])
+  @Query(() => [LessonType])
   lessons() {
     return this.lessonService.getLessons();
   }
 
-  @Query(type => LessonType)
+  @Query(() => LessonType)
   lesson(@Args('id') id: string) {
     return this.lessonService.getLesson(id);
   }
 
-  @Mutation(type => LessonType)
+  @Mutation(() => LessonType)
   createLesson(
     @Args('createLessonInput') createLessonInput: CreateLessonInput,
   ) {
     return this.lessonService.createLesson(createLessonInput);
+  }
+
+  @Mutation(() => LessonType)
+  assignStudentsToLesson(
+    @Args('assignStudentsToLessonInput')
+    assignStudentsToLesson: AssignStudentsToLessonInput,
+  ) {
+    const { lessonId, studentsIds } = assignStudentsToLesson;
+    return this.lessonService.assignStudentsToLesson(lessonId, studentsIds);
+  }
+
+  @ResolveField()
+  async students(@Parent() lesson: LessonEntity) {
+    this.studentService.getManyStudents(lesson.students);
   }
 }
